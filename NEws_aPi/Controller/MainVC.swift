9aafb5f863f7d5a13c49be53ba9ad673627f2778
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-05-23 23:35:15 +0900

    tableView testcell

--- NEws_aPi/Controller/MainVC.swift
+++ NEws_aPi/Controller/MainVC.swift
@@ -7,11 +7,49 @@
 
 import Foundation
 import UIKit
+import SnapKit
+import Then
 
-class MainVC: UIViewController{
+class MainVC: UIViewController
+{
+   
+    let table = UITableView()
+    let testarr = ["a","b","c"]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(table)
+        attribute()
+        layout()
+        
+    }
     
+    func layout() {
+        table.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+        
+    }
     
+    func attribute() {
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.delegate = self
+        table.dataSource = self
+    }
     
+}
+
+extension MainVC:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return testarr.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = testarr[indexPath.row]
+        
+        return cell
+    }
 }
