6a21da2020d5a2bc8c8eefb7896e50198077321c
Heesang <80827042+Heeesang@users.noreply.github.com>
2022-05-27 11:17:33 +0900

    NewAPI

---
+++ NEws_aPi/Network/NewsAPI.swift
@@ -0,0 +1,57 @@
+//
+//  News.swift
+//  NEws_aPi
+//
+//  Created by 곽희상 on 2022/05/26.
+//
+
+import Moya
+
+public enum News {
+    
+    case articles(q: String, from: String, sortBy: String, apiKey: String)
+}
+
+extension News: TargetType {
+    
+    public var baseURL: URL{
+        guard let url = URL(string: "https://newsapi.org") else {
+            fatalError("base URL couldn't be configure")
+        }
+        
+        return url
+    }
+    
+    public var path: String {
+        switch self {
+        case .articles:
+            return "/v2/everything"
+        }
+    }
+    
+    public var method: Method {
+        switch self{
+        case .articles:
+            return .get
+        }
+    }
+    
+    public var sampleData: Data{
+        return Data()
+    }
+    
+    public var task: Task {
+        switch self {
+        case .articles(let q, let from, let sortBy, let apiKey):
+            return .requestParameters(parameters: ["q": q, "from": from, "sortBy" : sortBy, "apiKey" : apiKey], encoding: URLEncoding.default)
+        }
+    }
+    
+    public var headers: [String : String]? {
+        return nil
+    }
+    
+    
+   
+    
+}
